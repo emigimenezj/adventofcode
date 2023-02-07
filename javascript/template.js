8db4912c6ec61dbbed72008b9db93a25c3dd9260
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-06 23:13:39 -0300

    Add js template for challenges

---
+++ javascript/template.js
@@ -0,0 +1,29 @@
+// DATA
+const input = ``;
+const test = ``;
+const data = test;
+
+
+
+// GENERAL
+
+
+
+
+// PART 1
+function part1() {
+
+}
+
+
+
+// PART 2
+function part2() {
+
+}
+
+
+
+// OUTPUT
+part1();
+part2();
